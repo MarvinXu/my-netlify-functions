0f777b8de3e3d0750d7304c51b9fea72478edf62
Marvin <272077995@qq.com>
2022-12-05 20:38:12 +0800

    gbk test

---
+++ netlify/functions/gbk.js
@@ -0,0 +1,9 @@
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "content-type": "text/html; charset=gbk"
+    }
+    body: '<h1>你好</h1>',
+  };
+};
